6e2c794dffe6efce172e4c601946560810d1680c
Muhammad Adeel <adeelshaikh654@gmail.com>
2021-05-24 01:24:59 +0500

    First commit

---
+++ app.js
@@ -0,0 +1,94 @@
+// Q1
+
+// var fName = prompt("Enter First Name");
+// var lName = prompt("Enter Last Name");
+
+// alert("Welcome " + fName + " " + lName + " to the website" );
+
+// Q2
+
+// var mobileModel = prompt("Enter Your Favorite Mobile Model");
+// document.write("My favorite mobile is: " + mobileModel + "<br>");
+// document.write("Length of String: " + mobileModel.length);
+
+// Q3
+
+// var Country = "Pakistani";
+
+// for (var i = 0; i < Country.length; i++) {
+//     if(Country[i].toLowerCase() == "k"){
+//         console.log(i)
+//         break;
+//     }
+// }
+
+// Q4
+
+// var text = "hello world";
+
+// console.log(text.lastIndexOf("l"));
+
+// Q5
+
+// var text = "Pakistan";
+
+// console.log(text.charAt(3));
+
+// Q6
+
+// var fName = prompt("Enter First Name");
+// var lName = prompt("Enter Last Name");
+// alert("Welcome " + fName.concat(" " +lName) + " to the website" );
+
+// Q7
+
+// var city = "Hyderabad";
+// var cityReplace = "Islam"
+// var cityText = city.slice(5);
+// console.log(cityReplace + cityText);
+
+// Q8
+
+// var message = "Ali and Sami are best friends. They play cricket and football together.";
+
+// for (var i = 0; i < message.length; i++) {
+//     if (message.slice(i, i + 3) === "and") {
+//         message = message.slice(0, i) + "&" + message.slice(i + 3); 
+//     }
+// }
+// console.log(message)
+
+// Q9
+
+// var text = "472";
+// var num = Number(text);
+
+// console.log("Value: " + text)
+// console.log("Type: " + typeof(text))
+// console.log("Type: " + typeof(num))
+
+
+// Q10
+
+// var userInput = prompt("Enter any text");
+
+// alert(userInput.toUpperCase());
+
+// Q11
+
+// var userInput = prompt("Enter any text");
+
+// alert(userInput.charAt(0).toUpperCase() + userInput.slice(1));
+
+
+// Q12
+
+    // var num = 35.36;
+    // var text = String(num);
+    
+    // for (let i = 0; i < text.length; i++) {
+    //     if(text.slice(i,i + 1) == "."){
+    //         text = text.slice(0,i) + "" + text.slice(i+1)
+    //     }
+    // }
+    // console.log(text)
